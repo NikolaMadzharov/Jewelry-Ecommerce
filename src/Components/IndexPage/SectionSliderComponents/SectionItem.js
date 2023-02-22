13c363567f18d664c12ef3441b0de31c335056d3
Nikola Madzharov <nmadzharov1912@gmail.com>
2023-02-23 00:47:38 +0200

    Added more components

---
+++ src/Components/IndexPage/SectionSliderComponents/SectionItem.js
@@ -0,0 +1,30 @@
+// import React from "react";
+// import { SectionSliderImage } from "./SectionSliderImage";
+// import { SectionSliderDescription } from "./SectionSliderDescription";
+
+// export const SecionItem = (props) =>{
+//     return (
+//         <div className="carousel-item active">
+//         <div className="row">
+//           <div className="col-md-6">
+//             <div className="detail_box">
+//               <h2>
+//                 <span> New Collection</span>
+//                 <hr /> 
+//               </h2>
+//               <h1>
+//                 Jewellery
+//               </h1>
+//              <SectionSliderDescription description ={props.description} />
+              
+//             </div>
+//           </div>
+//           <div className="col-md-6">
+//             <div className="img-box">
+//             <SectionSliderImage image ={props.image} />
+//             </div>
+//           </div>
+//         </div>
+//       </div>
+//     )
+// }
